d4cd990d11d664767a3d87eb836ac5769b0aa3ec
Baptiste Michaud <baptiste.michaud@edu.ece.fr>
2022-11-14 14:03:22 +0100

    route

--- main.c
+++ main.c
@@ -24,6 +24,7 @@
 
 typedef struct {
     int x, y, largeur, hauteur;
+    int occupe;
     ALLEGRO_COLOR couleur;
 } Case;
 
@@ -40,9 +41,10 @@ void initCases(Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX]) {
         for (j = 0; j < NB_COLONNES_MAX; j++) {
             cases[i][j].largeur = 25;
             cases[i][j].hauteur = 25;
-            cases[i][j].x = 25 + j * cases[i][j].largeur;
-            cases[i][j].y = 25 + i * cases[i][j].hauteur;
+            cases[i][j].x = 625 + j * cases[i][j].largeur;
+            cases[i][j].y = 145 + i * cases[i][j].hauteur;
             cases[i][j].couleur = NOIR;
+            cases[i][j].occupe = 0;
         }
     }
 }
@@ -83,17 +85,12 @@ void afficherDate() {
     }
 }
 
-void afficherToolBox(ALLEGRO_FONT *text, ALLEGRO_FONT *textBold, ALLEGRO_BITMAP *water, ALLEGRO_BITMAP *argent,
-                     ALLEGRO_BITMAP *habitant, ALLEGRO_BITMAP *elec,
-                     ALLEGRO_BITMAP *setting, ALLEGRO_BITMAP *cabane, ALLEGRO_BITMAP *watercastle,
-                     ALLEGRO_BITMAP *usine,
-                     ALLEGRO_BITMAP *route, ALLEGRO_BITMAP *caserne, Info info) {
+void afficherRessources(Info info, ALLEGRO_FONT *text, ALLEGRO_BITMAP *water, ALLEGRO_BITMAP *argent,
+                        ALLEGRO_BITMAP *habitant, ALLEGRO_BITMAP *elec) {
     al_draw_filled_rectangle(0, 0, LARGEUR_PLATEAU, 90, GRIS_FONCE);
     al_draw_filled_rectangle(600, 10, 1706, 86, GRIS_CLAIR);
     al_draw_filled_rectangle(603, 13, 1703, 83, BLANC);
     al_draw_filled_rectangle(606, 16, 1700, 80, GRIS_CLAIR);
-
-    al_draw_filled_rectangle(0, 0, 450, HAUTEUR_PLATEAU, GRIS_FONCE);
 
     al_draw_scaled_bitmap(argent, 0, 0, al_get_bitmap_width(argent), al_get_bitmap_height(argent), 1450, 17, 60, 60, 0);
     al_draw_scaled_bitmap(elec, 0, 0, al_get_bitmap_width(elec), al_get_bitmap_height(elec), 1200, 17, 60, 60, 0);
@@ -105,6 +102,14 @@ void afficherToolBox(ALLEGRO_FONT *text, ALLEGRO_FONT *textBold, ALLEGRO_BITMAP 
     al_draw_textf(text, BLANC, 1270, 35, 0, ": %d", info.elec);
     al_draw_textf(text, BLANC, 1020, 35, 0, ": %d", info.eau);
     al_draw_textf(text, BLANC, 770, 35, 0, ": %d", info.nbhabitant);
+}
+
+void afficherToolBox(ALLEGRO_FONT *text, ALLEGRO_FONT *textBold,
+                     ALLEGRO_BITMAP *setting, ALLEGRO_BITMAP *cabane, ALLEGRO_BITMAP *watercastle,
+                     ALLEGRO_BITMAP *usine,
+                     ALLEGRO_BITMAP *route, ALLEGRO_BITMAP *caserne) {
+
+    al_draw_filled_rectangle(0, 0, 450, HAUTEUR_PLATEAU, GRIS_FONCE);
 
     al_draw_filled_circle(40, 40, 34, GRIS_CLAIR);
     al_draw_filled_circle(40, 40, 32, BLANC);
@@ -132,7 +137,6 @@ void afficherToolBox(ALLEGRO_FONT *text, ALLEGRO_FONT *textBold, ALLEGRO_BITMAP 
     al_draw_filled_circle(225, 600, 88, BLANC);
     al_draw_filled_circle(225, 600, 86, GRIS_CLAIR);
 
-
     al_draw_scaled_bitmap(route, 0, 0, al_get_bitmap_width(route), al_get_bitmap_height(route), 50, 125, 125, 125, 0);
     al_draw_scaled_bitmap(cabane, 0, 0, al_get_bitmap_width(cabane), al_get_bitmap_height(cabane), 240, 115, 160, 160,
                           0);
@@ -159,55 +163,28 @@ void afficherToolBox(ALLEGRO_FONT *text, ALLEGRO_FONT *textBold, ALLEGRO_BITMAP 
     al_draw_text(textBold, NOIR, 135, 957, 0, "Niveau -2");
 }
 
-void raffraichir() {
-
-}
-
-
 bool isInRect(int x, int y, int x1, int y1, int x2, int y2) {
     return (x >= x1 && x <= x2 && y >= y1 && y <= y2);
 }
 
-/*void dessinerRoutes(Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX]) {
-    assert (al_init());
-    assert(al_install_mouse());
-    assert(al_init_primitives_addon());
-    ALLEGRO_EVENT_QUEUE *queue = NULL;
-    ALLEGRO_EVENT event;
-    bool fin = false;
-    queue = al_create_event_queue();
-    assert(queue != NULL);
-    al_register_event_source(queue, al_get_mouse_event_source());
-    al_draw_filled_circle(120, 200, 90, GRIS_TRANSPARENT);
-    while (!fin) {
-        al_wait_for_event(queue, &event);
-        switch (event.type) {
-            case ALLEGRO_EVENT_MOUSE_BUTTON_UP : {
-                for (int i = 0; i < NB_LIGNES_MAX; ++i) {
-                    for (int j = 0; j < NB_COLONNES_MAX; ++j) {
-                        if (isInRect(event.mouse.x, event.mouse.y, cases[i][j].x, cases[i][j].y,
-                                     cases[i][j].x + HAUTEUR, cases[i][j].y + LARGEUR)) {
-                            al_draw_filled_rectangle(cases[i][j].x, cases[i][j].y, cases[i][j].x + HAUTEUR,
-                                                     cases[i][j].y + LARGEUR, NOIR);
-                        }
-                        if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 197, 267)){
-                            fin = true;
-                        }
-                    }
-                }
-            } break;
-        }
-    }
-}*/
 
 void dessinerCases(Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX]) {
     int i, j;
     for (i = 0; i < NB_LIGNES_MAX; i++) {
         for (j = 0; j < NB_COLONNES_MAX; j++) {
-            al_draw_rectangle(cases[i][j].x + 600, cases[i][j].y + 120, cases[i][j].x + cases[i][j].largeur + 600,
-                              cases[i][j].y + 120 + cases[i][j].hauteur, cases[i][j].couleur, 1);
+            al_draw_rectangle(cases[i][j].x, cases[i][j].y, cases[i][j].x + cases[i][j].largeur,
+                              cases[i][j].y + +cases[i][j].hauteur, cases[i][j].couleur, 1);
         }
     }
+    al_flip_display();
+}
+
+
+void raffraichir(Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX]) {
+    al_clear_to_color(BLANC);
+    //plateau(fplateau);
+    initCases(cases);
+    dessinerCases(cases);
     al_flip_display();
 }
 
@@ -221,6 +198,41 @@ void plateau(ALLEGRO_BITMAP *fplateau) {
     al_draw_bitmap(fplateau, 0, 0, 0);
     al_flip_display();
 }
+
+int routeRecursif(Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX], int x, int y) {
+    assert (al_init());
+    assert(al_init_primitives_addon());
+    assert(al_install_mouse());
+    ALLEGRO_EVENT_QUEUE *queue = NULL;
+    ALLEGRO_EVENT event;
+    queue = al_create_event_queue();
+    al_register_event_source(queue, al_get_mouse_event_source());
+
+    bool fin = false;
+    int diffX, diffY = 0;
+    int newX, newY = -100;
+    while (!fin) {
+        al_wait_for_event(queue, &event);
+        switch (event.type) {
+            case ALLEGRO_EVENT_MOUSE_AXES : {
+                diffX = x - event.mouse.x;
+                diffY = y - event.mouse.y;
+                if (diffX >= 0) {
+                    al_draw_filled_rectangle(x + LARGEUR, y, x + 2 * LARGEUR, y + HAUTEUR, NOIR);
+                    newX = x + LARGEUR;
+                    newY = y;
+                }
+                break;
+            }
+            case ALLEGRO_EVENT_MOUSE_BUTTON_UP : {
+                fin = true;
+            }
+        }
+        al_flip_display();
+    }
+    return routeRecursif(cases, newX, newY);
+}
+
 
 int main() {
     assert (al_init());
@@ -255,9 +267,6 @@ int main() {
     caserne = al_load_bitmap("../Images/caserne.png");
 
     text = al_load_ttf_font("../Fonts/calibri.ttf", 30, 0);
-    if (!text) {
-        printf("erreur");
-    }
     textBold = al_load_ttf_font("../Fonts/calibrib.ttf", 50, 0);
 
     queue = al_create_event_queue();
@@ -272,18 +281,29 @@ int main() {
     bool fin = false;
     bool menu = false;
     bool jeu = false;
+    bool finRoute = false;
+    bool test = false;
+    bool finCabane = false;
+
     int l = 0;
     int k = 1;
     int y = 2022;
-    display = al_create_display(LARGEUR_PLATEAU, HAUTEUR_PLATEAU);
-    assert(display != NULL);
+    int preCaseXRoute = -100;
+    int preCaseYRoute = -100;
+    int caseXActuRoute = -100;
+    int caseYActuRoute = -100;
+
+    int preCaseXCabane = -100;
+    int preCaseYCabane = -100;
+    int caseXActuCabane = -100;
+    int caseYActuCabane = -100;
 
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
     al_register_event_source(queue, al_get_mouse_event_source());
 
     al_set_window_title(display, "ECE City");
-    al_set_window_position(display, 0, 0);
+    al_set_window_position(display, -3, 0);
 
     Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX];
     Info info;
@@ -295,6 +315,7 @@ int main() {
             al_wait_for_event(queue, &event);
             switch (event.type) {
                 case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+                    menu = true;
                     fin = true;
                     break;
                 }
@@ -317,27 +338,134 @@ int main() {
             }
         }
         al_clear_to_color(BLANC);
-        plateau(fplateau);
+        //plateau(fplateau);
         initCases(cases);
         dessinerCases(cases);
-        afficherToolBox(text, textBold, eau, argent, habitant, elec, setting, cabane, watercastle, usine, route,
-                        caserne, info);
+        afficherRessources(info, text, eau, argent, habitant, elec);
+        afficherToolBox(text, textBold, setting, cabane, watercastle, usine, route, caserne);
+        al_flip_display();
         while (!jeu) {
             al_wait_for_event(queue, &event);
-            al_flip_display();
             switch (event.type) {
-                case ALLEGRO_EVENT_DISPLAY_CLOSE: {
-                    jeu = true;
-                    break;
-                }
                 case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
-                    if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 197, 267)) {
+                    if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 187, 267)) {
                         al_draw_filled_circle(120, 200, 90, GRIS_TRANSPARENT);
+                        while (!finRoute) {
+                            al_wait_for_event(queue, &event);
+                            switch (event.type) {
+                                case ALLEGRO_EVENT_MOUSE_AXES : {
+                                    for (int i = 0; i < NB_LIGNES_MAX; ++i) {
+                                        for (int j = 0; j < NB_COLONNES_MAX; ++j) {
+                                            if (isInRect(event.mouse.x, event.mouse.y, cases[i][j].x, cases[i][j].y,
+                                                         cases[i][j].x + HAUTEUR, cases[i][j].y + LARGEUR)) {
+                                                caseXActuRoute = cases[i][j].x;
+                                                caseYActuRoute = cases[i][j].y;
+                                                if (cases[i][j].occupe == 0) {
+                                                    al_draw_filled_rectangle(caseXActuRoute, caseYActuRoute,
+                                                                             caseXActuRoute + HAUTEUR,
+                                                                             caseYActuRoute + LARGEUR, NOIR);
+                                                    test = true;
+                                                    if (preCaseXRoute != cases[i][j].x ||
+                                                        preCaseYRoute != cases[i][j].y) {
+                                                        al_draw_filled_rectangle(preCaseXRoute, preCaseYRoute,
+                                                                                 preCaseXRoute + HAUTEUR - 1,
+                                                                                 preCaseYRoute + LARGEUR - 1, BLANC);
+                                                        preCaseXRoute = cases[i][j].x;
+                                                        preCaseYRoute = cases[i][j].y;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (isInRect(event.mouse.x, event.mouse.y, 625, 145, 1750, 1020) == false) {
+                                        al_draw_filled_rectangle(preCaseXRoute, preCaseYRoute,
+                                                                 preCaseXRoute + HAUTEUR - 1,
+                                                                 preCaseYRoute + LARGEUR - 1, BLANC);
+                                    }
+                                    break;
+                                }
+                                case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
+                                    if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 197, 267)) {
+                                        afficherToolBox(text, textBold, setting, cabane,
+                                                        watercastle, usine, route, caserne);
+                                        finRoute = true;
+                                    }
+                                    if (test == true && isInRect(event.mouse.x, event.mouse.y, 625, 145, 1750, 1020)) {
+                                        //routeRecursif(cases, caseXActuRoute, caseYActuRoute);
+                                    }
+                                    break;
+                                }
+                            }
+                            al_flip_display();
+                        }
                     }
-                }
-                case ALLEGRO_EVENT_DISPLAY_RESIZE: {
-                    al_flip_display();
-                    break;
+                    /*if (isInRect(event.mouse.x, event.mouse.y, 56, 746, 400, 820)) {
+                        plateau(fplateau);
+                        dessinerCases(cases);
+                        afficherToolBox(text, textBold, eau, argent, habitant, elec, setting, cabane, watercastle,
+                                        usine, route,
+                                        caserne, info);
+                    }
+                    if (isInRect(event.mouse.x, event.mouse.y, 56, 846, 400, 920)) {
+                        al_clear_to_color(GRIS_CLAIR);
+                        dessinerCases(cases);
+                        afficherToolBox(text, textBold, eau, argent, habitant, elec, setting, cabane, watercastle,
+                                        usine, route,
+                                        caserne, info);
+                    }
+                    if (isInRect(event.mouse.x, event.mouse.y, 56, 946, 400, 1020)) {
+                        al_clear_to_color(GRIS_FONCE);
+                        dessinerCases(cases);
+                        afficherToolBox(text, textBold, eau, argent, habitant, elec, setting, cabane, watercastle,
+                                        usine, route,
+                                        caserne, info);
+                    }*/
+
+                    if (isInRect(event.mouse.x, event.mouse.y, 263, 133, 397, 267)) {
+                        al_draw_filled_circle(330, 200, 90, GRIS_TRANSPARENT);
+                        while (!finCabane) {
+                            al_wait_for_event(queue, &event);
+                            switch (event.type) {
+                                case ALLEGRO_EVENT_MOUSE_AXES : {
+                                    for (int i = 0; i < NB_LIGNES_MAX; ++i) {
+                                        for (int j = 0; j < NB_COLONNES_MAX; ++j) {
+                                            if (isInRect(event.mouse.x, event.mouse.y, cases[i][j].x, cases[i][j].y,
+                                                         cases[i][j].x + HAUTEUR, cases[i][j].y + LARGEUR)) {
+                                                caseXActuCabane = cases[i][j].x;
+                                                caseYActuCabane = cases[i][j].y;
+                                                if (cases[i][j].occupe == 0) {
+                                                    al_draw_scaled_bitmap(cabane, 0, 0, al_get_bitmap_width(cabane),
+                                                                          al_get_bitmap_height(cabane), cases[i][j].x - LARGEUR,
+                                                                          cases[i][j].y - 2*HAUTEUR, 75,75, 0);
+
+                                                    if (preCaseXCabane != cases[i][j].x ||
+                                                        preCaseYCabane != cases[i][j].y) {
+                                                        preCaseXCabane = cases[i][j].x;
+                                                        preCaseYCabane = cases[i][j].y;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    break;
+                                }
+                                case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
+                                    if (isInRect(event.mouse.x, event.mouse.y, 263, 133, 397, 267)) {
+                                        afficherToolBox(text, textBold, setting, cabane,
+                                                        watercastle, usine, route, caserne);
+                                        finCabane = true;
+                                    }
+                                    break;
+                                }
+                            }
+                            al_flip_display();
+                        }
+                        break;
+                    }
+                    case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+                        jeu = true;
+                        break;
+                    }
                 }
             }
         }
@@ -345,4 +473,6 @@ int main() {
         al_destroy_event_queue(queue);
         al_destroy_timer(timer);
     }
+
 }
+
