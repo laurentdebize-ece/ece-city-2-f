bfbd5a48d3c237665ec1f0cc828d9a73fb6b41c1
Antoine DE JESUS <antoine.dejesus@edu.ece.fr>
2022-11-23 13:08:47 +0100

    routes pour effectuer viarges etc...

--- routes.c
+++ routes.c
@@ -6,6 +6,11 @@ void dessinerRoutes(int *dessin, Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX], Inf
                     ALLEGRO_BITMAP *routeCote, ALLEGRO_BITMAP *routeDroite, ALLEGRO_BITMAP *routeTE, ALLEGRO_BITMAP *routeTN, ALLEGRO_BITMAP *routeTO, ALLEGRO_BITMAP *TS, ALLEGRO_BITMAP *routeX, ALLEGRO_BITMAP *virageNE,
                     ALLEGRO_BITMAP *virageON, ALLEGRO_BITMAP *virageSE, ALLEGRO_BITMAP *virageSO,ALLEGRO_BITMAP *eau, ALLEGRO_BITMAP *argent,
                     ALLEGRO_BITMAP *habitant, ALLEGRO_BITMAP *elec) {
+
+
+
+
+
     assert (al_init());
     assert(al_init_primitives_addon());
     al_draw_circle(120, 200, 92, ROUGE, 2);
@@ -32,30 +37,30 @@ void dessinerRoutes(int *dessin, Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX], Inf
                     }
                     // route de côté placée si la route d'avant ou d'après sur l'axe x est déjà placée
                     if (cases[i - 1][j].occupe == 1 && cases[i + 1][j].occupe == 1) {
-                        al_draw_scaled_bitmap(routeCote, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20, 0);
-                        // revoir code bitmap axe h et w
+                        al_draw_bitmap(routeCote, cases[i][j].x, cases[i][j].y, 0);
                         cases[i][j].occupe = 1;
                     }
                     // route de côté placée si la route d'avant et d'après sur l'axe x est déjà placée
                     if (cases[i][j - 1].occupe == 1 || cases[i][j + 1].occupe == 1) {
-                        al_draw_scaled_bitmap(routeCote, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20, 0);
+                        al_draw_bitmap(routeCote, cases[i][j].x, cases[i][j].y, 0);
                         // revoir code bitmap axe h et w
                         cases[i][j].occupe = 1;
                     }
                     // route droite (de haut) placée si la route d'avant ou d'après sur l'axe y est déjà placée
                     if (cases[i][j - 1].occupe == 1 && cases[i][j + 1].occupe == 1) {
-                        al_draw_scaled_bitmap(routeCote, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20, 0);
+                        al_draw_bitmap(routeDroite, cases[i][j].x, cases[i][j].y, 0);
                         // revoir code bitmap axe h et w
                         cases[i][j].occupe = 1;
                     }
                     // route droite (de haut) placée si la route d'avant et d'après sur l'axe y est déjà placée
                     if (cases[i][j - 1].occupe == 1 && cases[i][j + 1].occupe == 0 && cases[i - 1][j].occupe == 1 &&
                         cases[i + 1][j].occupe == 0) {
-                        al_draw_scaled_bitmap(virageBas, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20, 0);
-                        // revoir code bitmap axe h et w
+                        al_draw_bitmap(virageON, cases[i][j].x, cases[i][j].y, 0);
                         cases[i][j].occupe = 1;
                     }
                     // route virage vers le bas si sur axe x on a la case d'avant occupée et case d'après vide, idem pour axe y
+
+                    /*
                     if (cases[i][j - 1].occupe == 0 && cases[i + 1][j].occupe == 0 && cases[i - 1][j].occupe == 1 &&
                         cases[i][j + 1].occupe == 1) {
                         al_draw_scaled_bitmap(virageHaut, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20, 0);
@@ -63,37 +68,45 @@ void dessinerRoutes(int *dessin, Case cases[NB_LIGNES_MAX][NB_COLONNES_MAX], Inf
                         cases[i][j].occupe = 1;
                     }
                     // route virage vers le haut si sur axe x on a la case d'avant libre et case d'après occupée, inverse pour axe y
+
+                     */
+                    if (cases[i][j+1].occupe == 1 && cases[i][j - 1].occupe == 0 && cases[i + 1][j].occupe == 0 &&
+                        cases[i - 1][j].occupe == 0) {
+                        al_draw_bitmap(virageNE, cases[i][j].x, cases[i][j].y, 0);
+                        cases[i][j].occupe = 1;
+                    }
+                    if (cases[i][j+1].occupe == 0 && cases[i][j - 1].occupe == 1 && cases[i + 1][j].occupe == 1 &&
+                        cases[i - 1][j].occupe == 0) {
+                        al_draw_bitmap(virageSE, cases[i][j].x, cases[i][j].y, 0);
+                        cases[i][j].occupe = 1;
+                    }
+
                     if (cases[i][j - 1].occupe == 1 && cases[i][j + 1].occupe == 1 && cases[i + 1][j].occupe == 1 &&
                         cases[i - 1][j].occupe == 1) {
-                        al_draw_scaled_bitmap(croisementQuatres, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20,
-                                              20, 0);
-                        // revoir code bitmap axe h et w
+                        al_draw_bitmap(routeX, cases[i][j].x, cases[i][j].y, 0);
                         cases[i][j].occupe = 1;
                     }
                     // si toutes cases autour est occupée alors faire croisement 4
                     if (cases[i][j - 1].occupe == 1 && cases[i][j + 1].occupe == 0 && cases[i + 1][j].occupe == 1 &&
                         cases[i - 1][j].occupe == 1) {
-                        al_draw_scaled_bitmap(croisementTBas, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20,
-                                              0);
-                        // revoir code bitmap axe h et w
+                        al_draw_bitmap(routeTO, cases[i][j].x, cases[i][j].y, 0);
                         cases[i][j].occupe = 1;
                     }
                     // croisements vers le bas si route à droite à gauche et en bas occupées
                     if (cases[i][j - 1].occupe == 0 && cases[i][j + 1].occupe == 1 && cases[i + 1][j].occupe == 1 &&
                         cases[i - 1][j].occupe == 1) {
-                        al_draw_scaled_bitmap(croisementTHaut, 529, 337, 209, 118, cases[i][j].x, cases[i][j].y, 20, 20,
-                                              0);
-                        // revoir code bitmap axe h et w
+                        al_draw_bitmap(routeTN, cases[i][j].x, cases[i][j].y, 0);
                         cases[i][j].occupe = 1;
                     }
                     // croisements vers le haut si route à droite à gauche et en haut occupées                 }
+                }
             }
-        }
-        if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 197, 267)) {
-            afficherRessources(info, text, eau, argent, habitant, elec);
-            afficherToolBox(text, textBold, setting, cabane,
-                            watercastle, usine, routeX, caserne);
-            *dessin = -1;
+            if (isInRect(event.mouse.x, event.mouse.y, 53, 133, 197, 267)) {
+                afficherRessources(info, text, eau, argent, habitant, elec);
+                afficherToolBox(text, textBold, setting, cabane,
+                                watercastle, usine, routeX, caserne);
+                *dessin = -1;
+            }
         }
     }
 }
